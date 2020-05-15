16e8c09cec4c1b6d4d53d308a0aee41e20d1d584
alrus2797 <alrus2797@gmail.com>
2020-05-15 17:17:34 -0500

    "Change modules names"

---
+++ evolution_estrats/es11.py
@@ -0,0 +1,62 @@
+import math
+import numpy as np
+from utils import bio_utils as bu, es11_utils as es
+from tabulate import tabulate
+import sys
+
+def f(x,y):
+	return -math.cos(x)*math.cos(y)*math.exp(-(x-math.pi)**2-(y-math.pi)**2)
+
+sep = '-----------------------------'
+default_params = {
+	'n_poblation'   : 1,
+	'n_dimension'   : 2,
+	'iterations'	: 500,
+	'sigma'         : 1,
+	'range'			: (-10,10),
+}
+
+def run(new_params, stdout = None):
+	if not stdout: 
+			stdout = sys.stdout	
+
+	print('Parametros:')
+	print(tabulate(default_params.items(),headers= ['Name','Value'],tablefmt='orgtbl'),'\n')
+	default_params.update(new_params)
+	params = type('new_dict', (object,), default_params)
+
+	poblation	= np.random.uniform(*params.range,(params.n_poblation, params.n_dimension))
+	sigmas		= np.full((params.n_poblation, params.n_dimension), params.sigma).astype('float64')
+	fitness		= list(map(lambda x : f(*x), poblation))
+	print('Poblacion Inicial:')
+	print(tabulate(poblation, headers=['x','y'],tablefmt='orgtbl', showindex='always'),'\n')
+
+	for iteration in range(params.iterations):
+		print('|'+sep, f'Iteracion {iteration + 1}', sep+'|','\n')
+		print('Poblacion Actual - Calculo de aptitud:')
+		print(tabulate(np.column_stack((poblation, sigmas ,fitness)), headers= ['x','y', 'sigma1','sigma2','f(x,y)'],tablefmt='orgtbl', showindex='always'),'\n')
+
+		for idx, (subject, sub_fitness, sigma) in enumerate(zip(poblation, fitness, sigmas)):
+			candidate	= es.get_candidate(subject, sigma, params.range, verbose = True)
+			cte_fitness	= f(*candidate)
+			print(f'{np.around(candidate,5)} -> {cte_fitness}', end='')
+			if cte_fitness <= sub_fitness:
+				poblation[idx]	= candidate
+				fitness[idx]	= cte_fitness
+				sigmas[idx]		*= 1.5
+				print(' <- Sobrevive')
+			else:
+				sigmas[idx]		*= 0.9036
+				print(' <- No sobrevive')
+			print()
+
+		temp = sys.stdout
+		sys.stdout = stdout
+		best_subject_idx = np.argmin(fitness)
+		
+		print(f"\tIteration: {iteration + 1}/{params.iterations} - Best subject {poblation[best_subject_idx]} -> {fitness[best_subject_idx]:.5f}",end='\r')
+		
+		sys.stdout = temp		
+
+	print('Poblacion Final:')
+	print(tabulate(np.column_stack((poblation, sigmas ,fitness)), headers= ['x','y', 'sigma1','sigma2','f(x,y)'],tablefmt='orgtbl', showindex='always'),'\n')
--- menu.py
+++ menu.py
@@ -1,45 +1,54 @@
 import sys
+from termcolor import colored
 
 class Menu:
     def i1_reales(self, params, std = None):
-        import lab1.reals
+        import genetic_algs.reals
     def i2_tsp(self, params, std = None):
-        import lab1.tsp
+        import genetic_algs.tsp
     def i3_es11(self, params, std = None):
-        import lab2.es11
+        import evolution_estrats.es11 as mu_1
+        mu_1.run(params, std)
+
     def i4_esu1(self, params, std = None):
-        import lab2.esu1 as mu_lambda
+        import evolution_estrats.esu1 as mu_lambda
         mu_lambda.run(params, std)
     options = [
         {
             'name':'reales',
-            'function': 0,
+            'action': 0,
             'params': {}
         },
         {
             'name':'tsp',
-            'function': 1,
+            'action': 1,
             'params': {}
         },
         {
             'name':'(1 + 1)',
-            'function': 2,
+            'action': 2,
             'params': {}
         },
         {
             'name':'(mu + 1)',
-            'function': 3,
+            'action': 3,
             'params': {'_lambda': 1}
         },
         {
             'name':'(mu + lambda)',
-            'function': 3,
-            'params': {'_lambda': 6}
+            'action': 3,
+            'params': {
+                'iterations': 250,
+                '_lambda': 6
+            }
         },
         {
             'name':'(mu, lambda)',
-            'function': 3,
-            'params': {'_lambda': 12}
+            'action': 3,
+            'params': {
+                'iterations': 100,
+                '_lambda': 12
+            }
         },
     ]
     def run(self):
@@ -50,13 +59,14 @@ class Menu:
         option_choosed = self.options[idx_choosed]
 
 
-        f = getattr(self,methods[option_choosed['function']])
+        f = getattr(self,methods[option_choosed['action']])
         params = option_choosed['params']
-
+        print(f'Executing: {option_choosed["name"]} strategy')
         console_stdout      = sys.stdout
         sys.stdout  = open(f'output/{option_choosed["name"]}.txt', 'w')
         f(params, console_stdout)
 
         sys.stdout  = console_stdout
-        print(f'\nDone, check file: output/{option_choosed["name"]}.txt')
+        print(colored('\n','blue'))
+        print(colored(f'Done, check file: output/{option_choosed["name"]}.txt','green'))
             
