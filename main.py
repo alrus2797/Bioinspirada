cc1ae80df14f88f5bbb1cb5acd4ed5734d77339a
alrus2797 <alrus2797@gmail.com>
2020-05-14 23:50:09 -0500

    Add Pretty Menu (dev side)

--- main.py
+++ main.py
@@ -2,43 +2,10 @@ import sys
 
 print('Please run with Python: ^3.6.5')
 
+from menu import Menu
 
-
-options = [
-    '(1 + 1)',
-    '(mu + 1)',
-    '(mu + lambda)',
-    '(mu, lambda)',
-    ]
-
-if len(sys.argv) < 2:
-    stdout = sys.stdout
-    for idx, option in enumerate(options):
-        print(f'{idx+1}.- {option}')
-    choosed = int(input('Please choose an option: '))
-
-    filename = f'output/{options[choosed - 1]}.txt'
-    if choosed == 1:
-        sys.stdout = open(filename, 'w')
-        import lab2.es11
-    else:
-        if choosed == 2:
-            sys.stdout = open(filename, 'w')
-            import lab2.esu1 as es
-            es.run({'_lambda' : 1})
-        if choosed == 3:
-            sys.stdout = open(filename, 'w')
-            import lab2.esu1 as es
-            es.run({'_lambda' : 6})
-        if choosed == 4:
-            sys.stdout = open(filename, 'w')
-            import lab2.esu1 as es
-            es.run({'_lambda' : 12})
-        else:
-            print('Wrong!')        
-    sys.stdout = stdout
-    print('\nDone, check file:', filename)
-
+menu = Menu()
+menu.run()
 
     
 
--- menu.py
+++ menu.py
@@ -12,47 +12,54 @@ class Menu:
         mu_lambda.run(params)
         
 
-    options = {
-        0: {
+    options = [
+        {
             'name':'reales',
+            'function': 0,
             'params': {}
         },
-        1: {
+        {
             'name':'tsp',
+            'function': 1,
             'params': {}
         },
-        2: {
+        {
             'name':'(1 + 1)',
+            'function': 2,
             'params': {}
         },
-        3: {
+        {
             'name':'(mu + 1)',
+            'function': 3,
             'params': {'_lambda': 1}
         },
-        3: {
+        {
             'name':'(mu + lambda)',
+            'function': 3,
             'params': {'_lambda': 6}
         },
-        3: {
+        {
             'name':'(mu, lambda)',
+            'function': 3,
             'params': {'_lambda': 12}
         },
-    }
+    ]
 
     def run(self):
         methods = [ m for m in dir(Menu) if not m.startswith('__') ]
-        for function_key, option in self.options.items():
-            print(f'{function_key + 1}.- {option["params"]}')
+        for function_key, option in enumerate(self.options):
+            print(f'{function_key + 1}.- {option["name"]}')
         idx_choosed = int(input('Ingrese una opcion: ')) - 1
-        f = methods[idx_choosed]
-    
         option_choosed = self.options[idx_choosed]
-        
+
+
+        f = getattr(self,methods[option_choosed['function']])
         params = option_choosed['params']
 
         stdout      = sys.stdout
         sys.stdout  = open(f'output/{option_choosed["name"]}.txt', 'w')
         f(params)
+
         sys.stdout  = stdout
         print(f'\nDone, check file: output/{option_choosed["name"]}.txt')
         
